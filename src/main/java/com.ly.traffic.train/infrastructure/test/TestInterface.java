ac5344be7eba9c78394d4f5278afcfcef238d968
gaowei.liu <gaowei.liu@ly.com>
2020-09-02 19:47:30 +0800

    v2.0

---
+++ src/main/java/com.ly.traffic.train/infrastructure/test/TestInterface.java
@@ -0,0 +1,13 @@
+package com.ly.traffic.train.infrastructure.test;
+
+import com.ly.traffic.middleplatform.apt.annotation.Aggregate;
+
+/**
+ * @author liugw
+ * @Package com.ly.traffic.train.infrastructure.test
+ * @Description: ${TODO}
+ * @date 2020/9/2 19:45
+ */
+//@Aggregate
+public interface TestInterface {
+}
